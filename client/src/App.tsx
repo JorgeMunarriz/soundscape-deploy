51d107cac82e1324e82901f90c8b9a77e12fff99
Jorge Munárriz Gutiérrez-Solana <munarrizjorge@gmail.com>
2023-11-21 19:41:03 +0100

    fix(font face)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,25 @@
-import './App.css';
-import { Router } from './router/RouterPaths.routes';
-import './styles/font.css'
-import { UserProvider } from './context';
-import { GenresProvider } from './context/GenresContext';
-import { UserMusicProvider } from './context/UserMusicContext';
-import { QueuePlayerProvider } from './context/QueuePlayerContext';
+import "./App.css";
+import { Router } from "./router/RouterPaths.routes";
+// import './styles/font.css'
+import { UserProvider } from "./context";
+import { GenresProvider } from "./context/GenresContext";
+import { UserMusicProvider } from "./context/UserMusicContext";
+import { QueuePlayerProvider } from "./context/QueuePlayerContext";
 
 function App() {
-
-	return (
-		<>
-			<UserMusicProvider>
-				<UserProvider>
-					<GenresProvider>
-						<QueuePlayerProvider>
-							<Router />							
-						</QueuePlayerProvider>
-					</GenresProvider>
-				</UserProvider>
-			</UserMusicProvider>
-
-		</>
-	);
+  return (
+    <>
+      <UserMusicProvider>
+        <UserProvider>
+          <GenresProvider>
+            <QueuePlayerProvider>
+              <Router />
+            </QueuePlayerProvider>
+          </GenresProvider>
+        </UserProvider>
+      </UserMusicProvider>
+    </>
+  );
 }
 
 export default App;
