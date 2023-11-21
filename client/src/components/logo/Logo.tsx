6f18c5958638350d3724c13dcb816548aa12efab
Jorge Munárriz Gutiérrez-Solana <munarrizjorge@gmail.com>
2023-11-21 19:55:56 +0100

    New import logo

--- client/src/components/logo/Logo.tsx
+++ client/src/components/logo/Logo.tsx
@@ -1,9 +1,10 @@
 import {ContainerLogo} from './logo.styles';
+import { default as logo} from '../../assets/img/logo-homepage.png'
 
 export const Logo = () => {
 	return (
 		<ContainerLogo>
-			<img src="src\assets\img\logo-homepage.png" alt="logo" />
+			<img src={logo} alt="logo" />
 		</ContainerLogo>
 	);
 };
